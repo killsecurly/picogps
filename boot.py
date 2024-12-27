6cf2bd067c7df8978fd2493d7e62051f12a3cc43
KillSecurly Foundation <159317143+killsecurly@users.noreply.github.com>
2024-12-27 10:19:15 -0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -2,7 +2,7 @@ import board
 import digitalio
 import storage
 
-switch = digitalio.DigitalInOut(board.GP0)
+switch = digitalio.DigitalInOut(board.GP15)
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
